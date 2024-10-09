7ad2907b4ef45fcdf14adc15c8b6b3b01994e85b
hzx-123-wq <hanzixian1234@163.com>
2024-10-09 14:43:52 +0800

    Update val-tiny.py

--- val-tiny.py
+++ val-tiny.py
@@ -3,12 +3,12 @@ warnings.filterwarnings('ignore')
 from ultralytics import RTDETR
 
 if __name__ == '__main__':
-    model = RTDETR('runs/distill-tiny/rtdetr-logloss-cwd-exp2/weights/best.pt')
+    model = RTDETR('runs/train-tinyperson/your name/weights/best.pt')
     model.val(data='ultralytics/cfg/datasets/tinyperson.yaml',
               split='test',
               imgsz=640,
               batch=1,
             #   save_json=True, # if you need to cal coco metrice
-              project='runs/val-dis-tin',
-              name='rtdetr-logloss-cwd-exp2',
+              project='runs/val-tinyperson',
+              name='your name',
               )
