6a4d6a23f36658373fd54ce319344b90d766b7a2
hzx-123-wq <hanzixian1234@163.com>
2024-10-09 14:42:42 +0800

    Update val.py

--- val.py
+++ val.py
@@ -3,12 +3,12 @@ warnings.filterwarnings('ignore')
 from ultralytics import RTDETR
 
 if __name__ == '__main__':
-    model = RTDETR('runs/train/detr-SOEP-TEST/weights/best.pt')
+    model = RTDETR('runs/train/your name/weights/best.pt')
     model.val(data='ultralytics/cfg/datasets/VisDrone.yaml',
               split='test',
               imgsz=640,
               batch=1,
             #   save_json=True, # if you need to cal coco metrice
               project='runs/val',
-              name='detr-SOEP-TEST',
+              name='your name',
               )
